68d3a0759ae5e5483a555c321e7ebdcd889398d6
QAD6675 <qadirixx01@gmail.com>
2025-01-11 08:47:18 +0100

    my branch got messed up sorry

--- Assets/Scripts/Player/Movement/Teleporter.cs
+++ Assets/Scripts/Player/Movement/Teleporter.cs
@@ -1,7 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour {//created to manage any teleport logic/animation/effects..
+    public float cooldownLength=0.5f;
+    private bool onCooldown=false;
     public void teleport(Vector3 portal){
+        if(onCooldown) return;
         transform.position = portal;
+        onCooldown=true;
+        StartCoroutine(cooldown());
+    }
+    IEnumerator cooldown(){
+        yield return new WaitForSeconds(cooldownLength);
+        onCooldown=false;
     }
 }
